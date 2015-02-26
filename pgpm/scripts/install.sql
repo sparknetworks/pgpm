c38adb9a32f7e59160f3285c86488c202c109d5c
Artem Panchoyan <artem.panchoyan@gmail.com>
2015-02-26 18:59:33 +0100

    #1

--- pgpm/scripts/install.sql
+++ pgpm/scripts/install.sql
@@ -2,29 +2,6 @@ DO
 $$BEGIN
     -- Schema will have all info regarding packages. Name, version, dependencies, etc.
     CREATE SCHEMA _pgpm;
-    -- Version of package. Tries to follow semver as much as possible considering limited allowed characters
-    -- for schema name
-    CREATE TYPE _pgpm.package_version as
-    (
-        major smallint,
-        minor smallint,
-        patch smallint,
-        pre character varying(255),
-        metadata character varying(255)
-    );
-    -- class of package. Can refer to function/types schema, DDL schema
-    CREATE TABLE _pgpm.package_classes
-    (
-        pkg_c_id serial NOT NULL,
-        pkg_c_name character varying(255),
-        pkg_c_created timestamp without time zone DEFAULT now(),
-        pkg_c_last_modified timestamp without time zone DEFAULT now(),
-        CONSTRAINT package_class_pkey PRIMARY KEY (pkg_c_id)
-    );
-    INSERT INTO _pgpm.package_classes (pkg_c_name)
-        VALUES ('postgres_sql');
-    INSERT INTO _pgpm.package_classes (pkg_c_name)
-        VALUES ('postgres_ddl');
 
     -- subclass of package. Can refer either versioned schema (that adds suffix at the end) or non-versioned
     CREATE TABLE _pgpm.package_subclasses
@@ -46,13 +23,15 @@ $$BEGIN
         pkg_id serial NOT NULL,
         pkg_name character varying(255),
         pkg_description text,
-        pkg_version _pgpm.package_version,
-        pkg_class integer,
+        pkg_v_major smallint,
+        pkg_v_minor smallint,
+        pkg_v_patch smallint,
+        pkg_v_pre character varying(255),
+        pkg_v_metadata character varying(255),
         pkg_subclass integer,
         pkg_created timestamp without time zone DEFAULT now(),
         pkg_last_modified timestamp without time zone DEFAULT statement_timestamp(),
         CONSTRAINT package_pkey PRIMARY KEY (pkg_id),
-        CONSTRAINT package_class_fkey FOREIGN KEY (pkg_class) REFERENCES _pgpm.package_classes (pkg_c_id),
         CONSTRAINT package_subclass_fkey FOREIGN KEY (pkg_subclass) REFERENCES _pgpm.package_subclasses (pkg_sc_id)
     );
 
