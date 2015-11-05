7c6cce8499e2b6183d5994d26368db92e3f9b78a
Artem Panchoyan <artem.panchoyan@gmail.com>
2015-11-05 19:29:08 +0100

    uninstall testing

--- pgpm/settings.py
+++ pgpm/settings.py
@@ -7,7 +7,7 @@ PGPM_SCHEMA_NAME = '_pgpm'
 PGPM_SCHEMA_SUBCLASS = 'basic'
 PGPM_VERSION = config.Version(__version__, config.VersionTypes.python)
 
-MIGRATIONS_FOLDER_NAME = 'scripts/migrations'
+MIGRATIONS_FOLDER_NAME = 'lib/db_scripts/migrations'
 CONFIG_FILE_NAME = 'config.json'
 
 LOGGING_FORMATTER = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
