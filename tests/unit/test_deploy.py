7ee81f30f80aba72564eb97696e8c6887c67bd32
Artem Panchoyan <artem.panchoyan@gmail.com>
2015-11-09 17:52:20 +0100

    testing deployment

--- tests/unit/test_deploy.py
+++ tests/unit/test_deploy.py
@@ -7,5 +7,7 @@ import pgpm.lib.install
 
 class TestDeploymentManager:
 
-    def test_deploy_schema_to_db(self, deployment_manager):
+    def test_deploy_schema_to_db(self, installation_manager, deployment_manager):
+        assert installation_manager.install_pgpm_to_db(None) == 0
         assert deployment_manager.deploy_schema_to_db() == 0
+        assert installation_manager.uninstall_pgpm_from_db() == 0
