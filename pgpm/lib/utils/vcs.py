6c98c21e5cc98963fbf62ea4a752317c5dc47882
Artem Panchoyan <artem.panchoyan@gmail.com>
2016-12-22 18:22:01 +0100

    working on #41

--- pgpm/lib/utils/vcs.py
+++ pgpm/lib/utils/vcs.py
@@ -1,5 +1,7 @@
 import os
 import subprocess
+import dulwich.repo
+import dulwich.errors
 
 
 def is_git_directory(path='.'):
@@ -8,11 +10,12 @@ def is_git_directory(path='.'):
     :param path: path to check
     :return: True if it's a git repo and False otherwise
     """
-    if path and subprocess.call(['git', '-C', path, 'rev-parse', '--is-inside-work-tree'],
-                           stderr=subprocess.STDOUT, stdout=open(os.devnull, 'w')) == 0:
-        return True
-    else:
+    try:
+        dulwich.repo.Repo.discover(path)
+    except dulwich.errors.NotGitRepository:
         return False
+
+    return True
 
 
 def get_git_revision_hash(path='.'):
@@ -21,7 +24,7 @@ def get_git_revision_hash(path='.'):
     :param path: path to repo
     :return: hash or exception
     """
-    return subprocess.check_output(['git', '-C', path, 'rev-parse', 'HEAD']).strip().decode('utf-8')
+    return dulwich.repo.Repo.discover(path).head()
 
 
 def get_git_remote(path='.', remote='origin', type='Fetch'):
@@ -32,6 +35,7 @@ def get_git_remote(path='.', remote='origin', type='Fetch'):
     :param type:
     :return: remote url or exception
     """
+    repo = dulwich.repo.Repo.discover(path)
     str = subprocess.check_output(['git', '-C', path, 'remote', 'show', '-n', remote]).strip().decode('utf-8')
     str = str[str.find(type):]
     str = str[:str.find('\n')].split(' ')[-1]
